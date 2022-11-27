e1194d001f68664915b02de7786631533fdb968a
Vitor Bortoloti <bortoloti.vitor@gmail.com>
2022-11-27 17:22:45 -0300

    Adding fist modifications for prism

---
+++ App/App/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Prism.DryIoc;
+using Prism.Ioc;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace App
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
