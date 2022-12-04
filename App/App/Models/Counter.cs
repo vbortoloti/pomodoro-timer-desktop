6d25ecb5a3917708901c541f6e20f2e6d71eb4a0
Vitor Bortoloti <bortoloti.vitor@gmail.com>
2022-12-03 21:09:42 -0300

    Adding counter

---
+++ App/App/Models/Counter.cs
@@ -0,0 +1,42 @@
+﻿using App.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace App.Models
+{
+    public class Counter
+    {
+        public int Count { get; private set; } = 300;
+        public DispatcherTimer Timer;
+
+        private bool timerStarted = false;
+
+        CountDownViewModel CountView;
+        DateTime time = DateTime.Now;
+        public void InitializeTimer()
+        {
+            timerStarted = true;
+            Timer = new DispatcherTimer();
+            Timer.Interval = TimeSpan.FromMilliseconds(100);
+            Timer.Tick += TimerTick;
+            Timer.Start();
+            time = DateTime.Now;
+
+        }
+        void TimerTick(object sender, EventArgs e)
+        {
+            DateTime timeNow = DateTime.Now;
+            
+        }
+
+        public Counter(int timeInSeconds, CountDownViewModel countView)
+        {
+            Count = timeInSeconds;
+            CountView = countView;
+        }
+    }
+}
---
+++ App/App/ViewModels/CountDownViewModel.cs
@@ -0,0 +1,30 @@
+﻿using App.Models;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace App.ViewModels
+{
+    public class CountDownViewModel: BindableBase
+    {
+        private string _countText = "00:00";
+        public string CountText
+        {
+            get { return _countText; }
+            set { SetProperty(ref _countText, value); }
+        }
+
+        
+
+        public CountDownViewModel()
+        {
+            Counter counter = new Counter(1500,this);
+            counter.InitializeTimer();
+        }
+  
+    }
+}
